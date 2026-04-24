6a754e2412b62ca2fd7f528a8a95787a99c1bbe7
tharindueranga <tharindueranga3@gmail.com>
2026-04-24 13:32:16 +0400

    init git repository

---
+++ src/main/java/com/example/candleaggregation/CandleAggregationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.candleaggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandleAggregationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CandleAggregationServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/candleaggregation/CandleAggregationServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.candleaggregation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandleAggregationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
